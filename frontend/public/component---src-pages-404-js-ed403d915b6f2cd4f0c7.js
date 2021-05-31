4e05061753590b32e2a4ec51726b86c005cb7cfa
ivychxn <i1chan@ucsd.edu>
2021-05-30 18:56:00 -0700

    testing

---
+++ frontend/public/component---src-pages-404-js-ed403d915b6f2cd4f0c7.js
@@ -0,0 +1,17 @@
+(self["webpackChunkfrontend"] = self["webpackChunkfrontend"] || []).push([[883],{
+
+/***/ 9616:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7294);
+/* harmony import */ var gatsby__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5444);
+// styles
+var pageStyles={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"};var headingStyles={marginTop:0,marginBottom:64,maxWidth:320};var paragraphStyles={marginBottom:48};var codeStyles={color:"#8A6534",padding:4,backgroundColor:"#FFF4DB",fontSize:"1.25rem",borderRadius:4};// markup
+var NotFoundPage=function NotFoundPage(){return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main",{style:pageStyles},/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("title",null,"Not found"),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1",{style:headingStyles},"Page not found"),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p",{style:paragraphStyles},"Sorry"," ",/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span",{role:"img","aria-label":"Pensive emoji"},"\uD83D\uDE14")," ","we couldn\u2019t find what you were looking for.",/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br",null), false?/*#__PURE__*/0:null,/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br",null),/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__/* .Link */ .rU,{to:"/"},"Go home"),"."));};/* harmony default export */ __webpack_exports__["default"] = (NotFoundPage);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=component---src-pages-404-js-ed403d915b6f2cd4f0c7.js.map
